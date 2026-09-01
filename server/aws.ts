6cc0994de94544948d3b90accb7c391a7d28ab9f
karan <xkaranx5@gmail.com>
2026-09-01 06:37:44 +0530

    Add AWS S3 commands for bucket policy, public access block, and object listing

--- server/aws.ts
+++ server/aws.ts
@@ -1,4 +1,4 @@
-import { ListBucketsCommand } from "@aws-sdk/client-s3";
+import { GetBucketPolicyCommand, ListBucketsCommand, GetPublicAccessBlockCommand , ListObjectsV2Command, GetObjectCommand } from "@aws-sdk/client-s3";
 import { awsClient } from "./clients/awsClient";
 
 class Aws {
@@ -11,6 +11,57 @@ class Aws {
       console.log("Error", err);
     }
   };
+
+  public static readonly getPublicAccessBlock = async () => {
+    try {
+      const data = await awsClient.send(new GetPublicAccessBlockCommand({Bucket: "aidebate-cli"}));
+      console.log("Success", data.PublicAccessBlockConfiguration);
+      return data.PublicAccessBlockConfiguration;
+    } catch (err) {
+      console.log("Error", err);
+    }
+  };
+
+  public static readonly getBucketPolicy = async () => {
+    try {
+      const data = await awsClient.send(new GetBucketPolicyCommand({Bucket: "aidebate-cli"}));
+      console.log("Success", data.Policy);
+      return data.Policy;
+    } catch (err) {
+      console.log("Error", err);
+    }
+  };
+
+    public static readonly listObjects = async () => {
+    try {
+      const data = await awsClient.send(new ListObjectsV2Command({Bucket: "aidebate-cli"}));
+      console.log("Success", data.Contents);
+      return data.Contents;
+    } catch (err) {
+      console.log("Error", err);
+    }
+  };
+
+    public static readonly getObject = async (objectKey: string) => {
+    try {
+      const data = await awsClient.send(new GetObjectCommand({Bucket: "aidebate-cli", Key: objectKey}));
+      console.log("Success", data);
+     /*  data.Body?.transformToString().then((body) => {
+        console.log("Body", body);
+      }); */
+
+      data.Body?.on("data", (chunk) => {
+        console.log("Data", chunk);
+      });
+      return data;
+    } catch (err) {
+      console.log("Error", err);
+    }
+  };
+
 }
 
-await Aws.listBuckets();
+// await Aws.getPublicAccessBlock();
+// await Aws.getBucketPolicy();
+// await Aws.listObjects();
+await Aws.getObject("hello");
