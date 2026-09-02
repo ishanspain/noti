e69c40238e1c993f01f1cc2755e21b7b98b1b781
Ishan Sharma <ishan.spain@gmail.com>
2026-09-02 12:34:00 +0530

    Update AWS S3 bucket configuration and enhance presigned URL functions

--- server/aws.ts
+++ server/aws.ts
@@ -7,9 +7,11 @@ import {
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { awsClient } from "./clients/awsClient";
 
+const bucketName = "aidebate-cli";
+
 export async function createGetPresignedUrl(objectKey: string) {
   const command = new GetObjectCommand({
-    Bucket: "printcampus",
+    Bucket: bucketName,
     Key: objectKey,
   });
 
@@ -21,13 +23,16 @@ export async function createGetPresignedUrl(objectKey: string) {
 
 export async function createPutPresignedUrl(objectKey: string) {
   const command = new PutObjectCommand({
-    Bucket: "printcampus",
+    Bucket: bucketName,
     Key: objectKey,
+    ContentType: "video/mp4",
+
   });
 
   // console.log(command)
   return getSignedUrl(awsClient, command, {
     expiresIn: 15 * 60,
+    signableHeaders: new Set(["content-type"]),
   });
 }
 
@@ -35,6 +40,6 @@ export async function createPutPresignedUrl(objectKey: string) {
 /* const url = await createGetPresignedUrl(
   "uploads/1785992369681_spiral-binding.png",
 ); */
-const url = await createPutPresignedUrl("arun-thesis-flow-process.mp4");
+const url = await createPutPresignedUrl("test.mp4");
 
 console.log(url);
