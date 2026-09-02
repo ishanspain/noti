9cb7489f489b35bcfca0b3b8d61ca493562123f9
karan <xkaranx5@gmail.com>
2026-09-02 06:35:52 +0530

    Implement file upload functionality to AWS S3 using @aws-sdk/lib-storage

---
+++ server/aws-up.ts
@@ -0,0 +1,34 @@
+import { Upload } from "@aws-sdk/lib-storage";
+import { awsClient } from "./clients/awsClient";
+import { createReadStream } from "node:fs";
+
+/* const rs = createReadStream(
+  "/home/karan/Downloads/Thesis_animation_five_steps_202607260829.mp4",
+); */
+const rs = createReadStream(
+  "/home/karan/Videos/Ne Zha 2 2025 1080p WEB-DL HEVC x265 5.1 BONE.mkv",
+);
+
+try {
+  const upload = new Upload({
+    client: awsClient,
+    params: {
+      Bucket: "aidebate-cli",
+      Key: "test.mp4",
+      Body: rs,
+    },
+  });
+
+  upload.on("httpUploadProgress", (progress) => {
+    if(!progress.loaded || !progress.total) return
+    const precentage = Math.round((progress.loaded / progress.total) * 100);
+    process.stderr.write(`\r progress percentage, ${precentage}% `);
+  });
+
+  const res = await upload.done();
+
+//   console.log(res);
+//   console.log(upload);
+} catch (err) {
+  console.log("error upload file", err);
+}
--- server/clients/awsClient.ts
+++ server/clients/awsClient.ts
@@ -3,9 +3,9 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const awsClient = new S3Client({
   // endpoint,
-  // region: process.env.BB_REGION,
+  region: "ap-south-1",
   // credentials: {
-  //   accessKeyId: process.env.BB_KEY_ID,
-  //   secretAccessKey: process.env.BB_APP,
+  //   accessKeyId: process.env.AWS_KEY,
+  //   secretAccessKey: process.env.AWS_SECRET,
   // },
 });
